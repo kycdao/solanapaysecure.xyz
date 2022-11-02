a5e363c2a4bd17760a63adf47e48cb97183abc1f
gyorg <gyorgy.herbszt@sonardesign.hu>
2022-11-02 23:00:51 +0100

    gyuri: modal transition

--- src/views/home.js
+++ src/views/home.js
@@ -75,8 +75,7 @@ const Home = (props) => {
         </div>
       </div>
 
-      <div id="the-modal" className="">
-
+      <div id="the-modal" className="opened">
         <div className="modal-content">
           <div className="modal-header">
             <h1>Connect Wallet</h1>
@@ -85,19 +84,15 @@ const Home = (props) => {
               <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24"><path d="M6.4 19 5 17.6l5.6-5.6L5 6.4 6.4 5l5.6 5.6L17.6 5 19 6.4 13.4 12l5.6 5.6-1.4 1.4-5.6-5.6Z"/></svg>
             </div>
           </div>
-
           <div className="modal-body">
-
+            QR code goes here
           </div>
-
-
           <div className="modal-button">
             <span className="modal-text">
                 <span>Connect wallet</span>
             </span>
           </div>
         </div>
-
       </div>
 
     </div>
