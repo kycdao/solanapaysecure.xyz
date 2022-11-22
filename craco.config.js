aca951566264563adc90c48483693d132d945ef2
AttilaVargaw <attilavargaw@gmail.com>
2022-11-22 12:01:33 +0100

    Update websdk

--- craco.config.js
+++ craco.config.js
@@ -30,7 +30,8 @@ module.exports = {
 			],
 			resolve: {
 				fallback: {
-					crypto: require.resolve("crypto-browserify")
+					crypto: require.resolve("crypto-browserify"),
+					crypto: require.resolve("stream-browserify")
 				}
 			}
 		}
